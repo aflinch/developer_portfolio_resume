0e37e77d613d699651a9736928fd6e2463480e20
aflinch <alexanderflinchum@gmail.com>
2026-06-02 22:04:25 -0500

    updated icon, giving modal the ability to import it as a parameter

--- src/Components/SkillsModel.jsx
+++ src/Components/SkillsModel.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { createPortal } from 'react-dom';
 import { X } from 'lucide-react';
 
-function SkillsModel({ skill, onClose }) {
+function SkillsModel({ skill, iconComponent, onClose }) {
+    const IconComponent = iconComponent;
 
     return createPortal(
        <div className="modal-overlay" onClick={onClose}>
            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                <button className="modal-close" onClick={onClose}><X /></button>
                <div className="modal-body">
-                   <h3>{skill.title}</h3>
-                   <p>{skill.description}</p>
+                   <div className="model-header-section">
+                       <h3>{iconComponent ? <IconComponent /> : null} {skill.title}</h3>
+                       <p className="skills-section-card-progress-num">{skill.level}%</p>
+                   </div>
+                   <div className="skills-section-card-progress-bar">
+                       <div className="skills-section-card-progress" style={{ width: `${skill.level}%` }}></div>
+                   </div>
+                   <p className="model-description">{skill.description}</p>
                </div>
            </div>
        </div>,
