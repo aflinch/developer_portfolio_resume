1cba394339a80713a6b176590151e1656e284f45
aflinch <alexanderflinchum@gmail.com>
2026-06-03 13:12:27 -0500

    initiated page structure for contact section

---
+++ src/Pages/Contact/ContactSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ContactSection() {
+
+    return (
+        <section id="contactSection" className="contact-section">
+
+        </section>
+    )
+}
